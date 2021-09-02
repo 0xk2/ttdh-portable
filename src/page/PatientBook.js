290deb5df15479c69c0c5a9f697fbb68b5954f06
hieubt <hieubt@3dsigma.net>
2021-09-02 10:09:12 +0700

    wip: patientbook

--- src/page/PatientBook.js
+++ src/page/PatientBook.js
@@ -1,66 +1,70 @@
 import { Box, Container } from "@material-ui/core"
 import PatientList from "../component/PatientList"
-// import { SORT } from "../config/Constants";
+import { SORT } from "../config/Constants";
 import { useAuth } from "../context/AuthContext"
-// import { useEffect, useState } from "react";
-// import { ArrowUpward,ArrowDownward } from "@material-ui/icons";
-// import {get, getDatabase, ref, child} from "firebase/database"
-// import {getDatabase} from "firebase/database"
+import { useEffect, useState } from "react";
+import { ArrowUpward,ArrowDownward } from "@material-ui/icons";
+import {get, getDatabase, ref, child} from "firebase/database"
+import { isNil } from "lodash";
+import { useUIHelper } from "../context/UIHelperContext";
 
-// const db = getDatabase()
+const db = getDatabase()
 
-// function countNC(patients,nc) {
-//   let total = 0;
-//   Object.keys(patients).map(key => {
-//     total = patients[key].nc === nc ? total + 1:total
-//     return 0
-//   })
-//   return total
-// }
+function countNC(patients,nc) {
+  let total = 0;
+  Object.keys(patients).map(key => {
+    total = patients[key].nc === nc ? total + 1:total
+    return 0
+  })
+  return total
+}
 
 function PatientBook(props){
-  // const {userInfo} = useAuth()
-  // const [sortBy, sort] = useState(SORT.OLDEST)
-  // const [patients, setPatients] = useState({})
-  // let originalPatientKeys = userInfo === undefined?{}:userInfo.patients===undefined?{}:userInfo.patients;
-  // useEffect(() => {
-  //   Object.keys(originalPatientKeys).forEach((key) => {
-  //     get(child(ref(db),'/patients/'+key)).then((patientSnapShot) => {
-  //       const aPatient = {}
-  //       patientSnapShot.forEach((attrSnapshot) => {
-  //         if(attrSnapshot.key !== 'history'){
-  //           aPatient[attrSnapshot.key] = attrSnapshot.val()
-  //         }
-  //       })
-  //       console.log(key, aPatient)
-  //       setPatients((state, props) => {return {...state, key: aPatient}})
-  //     })
-  //   })
-  // },[originalPatientKeys])
-  
-  // return (
-  //   <Container maxWidth="md" className="frm-container PatientBook">
-  //     <Box className="title pt16">Danh sách bệnh nhân ({Object.keys(patients).length})</Box>
-  //     <Box className="controller">
-  //       <Box className="nc-counter">
-  //         {['nc0','nc1','nc2','nc3','nc4'].map(nc => {
-  //           return <span style={{marginRight: "5px"}}>{nc+': '+countNC(patients,nc)}</span>
-  //         })}
-  //       </Box>
-  //       <Box>
-  //         <div onClick={() => {sort(sortBy === SORT.LATEST?SORT.OLDEST:SORT.LATEST)}}>{sortBy === SORT.LATEST? <ArrowUpward />:<ArrowDownward />}</div>
-  //       </Box>
-  //     </Box>
-  //     {PatientList(patients, sortBy)}
-  //   </Container>
-  // )
   const {userInfo} = useAuth()
-  let patients = userInfo === undefined?{}:userInfo.patients;
-  patients = patients === undefined ?{}:patients
+  const [sortBy, sort] = useState(SORT.OLDEST)
+  const [patients, setPatients] = useState({})
+  const {setBackdropState} = useUIHelper()
+  let originalPatientKeys = userInfo === undefined?{}:userInfo.patients===undefined?{}:userInfo.patients;
+  const keys = Object.keys(originalPatientKeys)
+  console.log(keys)
+  useEffect(() => {
+    if(keys.length > 0){
+      async function fetchPatients(){
+        //setBackdropState(true)
+        const obj = {}
+        for(var i=0;i<keys.length;i++){
+          const key = keys[i]
+          const patientSnapShot = await get(child(ref(db),'/patients/'+key))
+          const aPatient = patientSnapShot.val()
+          if(isNil(aPatient)){
+            continue;
+          }
+          const timeStamps = []
+          Object.keys(aPatient.history).map(timestamp => timeStamps.push(parseInt(timestamp)))
+          const lastestTimestamp = Math.max(...timeStamps)
+          obj[key] = {...aPatient, nc: aPatient.history[lastestTimestamp+""].nc, "latestTimestamp": lastestTimestamp}
+        }
+        setPatients({...obj})
+        //setBackdropState(false)
+      }
+      fetchPatients()
+    }
+  },[])
+  
   return (
     <Container maxWidth="md" className="frm-container PatientBook">
-      <Box className="title pt16">Danh sách bệnh nhân</Box>
-      {PatientList(patients)}
+      <Box className="title pt16">Danh sách bệnh nhân ({Object.keys(patients).length})</Box>
+      <Box className="controller">
+        <Box className="nc-counter">
+          {['nc0','nc1','nc2','nc3','nc4'].map((nc,idx) => {
+            return <span key={idx} style={{marginRight: "5px"}}>{nc+': '+countNC(patients,nc)}</span>
+          })}
+        </Box>
+        <Box>
+          <div onClick={() => {sort(sortBy === SORT.LATEST?SORT.OLDEST:SORT.LATEST)}}>{sortBy === SORT.LATEST? <ArrowUpward />:<ArrowDownward />}</div>
+        </Box>
+      </Box>
+      {PatientList(patients, sortBy)}
     </Container>
   )
 }
